937c50adfd8c4c0e3ea3d6f1bea0784077257cbf
deddenblc <denys.cheberiak@nure.ua>
2021-04-19 19:43:29 +0300

    AddController

---
+++ BeFriendServer/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using BeFriendServer.Data;
+using BeFriendServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeFriendServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        private readonly befrienddbContext _context;
+
+        public ValuesController(befrienddbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.Users.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+    }
+}
